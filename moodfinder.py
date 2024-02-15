940693db2446f3e481e50d48afc5e7e5d32964c3
aa <142321494+AnandRajaM@users.noreply.github.com>
2024-02-15 21:51:35 +0530

    Update moodfinder.py

--- moodfinder.py
+++ moodfinder.py
@@ -17,13 +17,15 @@ def send_message(message):
     )
     return response['choices'][0]['message']['content']
 
+#WHEN YOU TAKE INPUT FORM USER IN WEBSITE JUST RUN THIS FUCNTUION WITH THE INOPUT!!! main()
 def main():
     
     print("Bliss: Hello!What's Up?")
 
     while True:
+        #INPUT FROM WEBSITE GOES HEREEEE
         user_input = input("You: ")
-        mood_inp = "what is the mood of this message, give me a reply in ONLY one word  :" + user_input
+        mood_inp = "what is the mood of the given message give me a reply ONLY from these categories [sad,joyful,angry,neutral,satisfied,grateful,depressed,disgusted,surpised,a pervert]  :" + user_input
         
         if user_input.lower() == 'exit':
             print("Bliss: Goodbye! Take care.")
